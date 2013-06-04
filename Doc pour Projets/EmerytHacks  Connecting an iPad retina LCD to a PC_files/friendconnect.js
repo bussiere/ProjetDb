84a28292fe0da7748b28c18560ec18f9710d413a
bussiere <bussiere@gmail.com>
2013-06-04 14:45:56 +0200

    doc ecran

---
+++ Doc pour Projets/EmerytHacks  Connecting an iPad retina LCD to a PC_files/friendconnect.js
@@ -0,0 +1,26 @@
+
+
+<html>
+<head>
+<link rel=stylesheet type=text/css href=http://proxy.infra.dgfip/errorpage.css />
+<meta http-equiv=Content-Type content=text/html> 
+<title>ERREUR: L'acc&egrave;s &agrave; l'url demand&eacute;e est interdit</title> 
+<style type=text/css>
+<!--  %l  body :lang(fa) { direction: rtl; font-size: 100%; font-family: Tahoma, Roya, sans-serif; float: right; } :lang(he) { direction: rtl; }  -->
+</style> 
+</head>
+
+
+<body id=%c> 
+<div id=content><img src=http://proxy.infra.dgfip/images/alert-reminder.png id=imgalert>
+<h2>L'acc&egrave;s &agrave; l'url demand&eacute;e est interdit</h2> 
+<p>Vous ne pouvez pas acc&eacute;der &agrave; l'URL : <a href=http://www.google.com/friendconnect/script/friendconnect.js>http://www.google.com/friendconnect/script/friendconnect.js</a></p>  <blockquote id=error> <p><b>Acc&egrave;s interdit.</b></p> 
+</blockquote><p>La configuration du contr&ocirc;le d'acc&egrave;s, emp&ecirc;che votre requ&ecirc;te d'&ecirc;tre accept&eacute;e. </p>
+<p>Toute demande d'acc&egrave;s &agrave; un site pour des raisons professionnelles doit &ecirc;tre adress&eacute;e, pour examen, par le chef de service sur la BALF de l'administrateur proxy ( <a href=mailto:proxys.internet@dgfip.finances.gouv.fr>proxys.internet@dgfip.finances.gouv.fr</a> )</p> 
+<p>Vous trouverez <a href=http://magellan.bercy.cp/communication/charte/charte.htm>ici</a> les r&egrave;gles de bons usages de l'internet d&eacute;finis dans la Charte d'usage des TIC</p>
+<div id=footer> <p>G&eacute;n&eacute;r&eacute;e le Tue, 4 Jun 2013 02:44:30 </p></div> 
+
+ </div>
+</body>
+
+</html>
