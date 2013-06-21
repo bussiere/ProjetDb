3487c633c93fdbef4e415e3b4af4c28b1f530f64
Bussiere <bussiere@gmail.com>
2013-06-21 12:07:13 +0200

    ligne droite

---
+++ ConferenceGits/Doc/avatar-2045_files/count-data.js
@@ -0,0 +1,5 @@
+var DISQUSWIDGETS;
+
+if (typeof DISQUSWIDGETS != 'undefined') {
+    DISQUSWIDGETS.displayCount({"showReactions":false,"text":{"and":"et","reactions":{"zero":"","multiple":"","one":""},"comments":{"zero":"0","multiple":"{num}","one":"1"}},"counts":[{"uid":0,"comments":158},{"uid":1,"comments":158}]});
+}
---
+++ ConferenceGits/Doc/avatar-2045_files/popular_threads_widget.js
@@ -0,0 +1,67 @@
+
+
+document.write(' \
+<style type="text/css" media="screen">\
+	 .dsq-widget ul.dsq-widget-list {\
+	 padding: 0;\
+	 margin: 0;\
+	 text-align: left;\
+	 }\
+	 img.dsq-widget-avatar {\
+	 border: 0px;\
+	 margin: 0px;\
+	 padding: 0px 3px 3px 0px;\
+	 float: left;\
+	 }\
+	 a.dsq-widget-user {\
+	 font-weight: bold;\
+	 }\
+	 a.dsq-widget-thread {\
+	 font-weight: bold;\
+	 }\
+	 p.dsq-widget-meta {\
+	 clear: both;\
+	 font-size: 80%;\
+	 padding: 0;\
+	 margin: 0;\
+	 }\
+	 li.dsq-widget-item {\
+	 margin: 15px 0;\
+	 list-style-type: none;\
+	 clear: both;\
+	 }\
+	 span.dsq-widget-clout {\
+	 padding: 0 2px;\
+	 background-color: #ff7300;\
+	 color: #fff;\
+	 }\
+	 table.dsq-widget-horiz td {\
+	 padding-right: 15px;\
+	 }\
+	 .dsq-widget-comment p {\
+	 display: inline;\
+	 }\
+	 </style>\
+	 <ul class="dsq-widget-list">\
+	 <li class="dsq-widget-item">\
+	 <a class="dsq-widget-thread" href="http://korben.info/joeoffice.html">30 jours pour coder un clone de Microsoft Office</a>\
+	 <p class="dsq-widget-meta">51 comments &middot; 1 hour ago</p>\
+	 </li>\
+	 <li class="dsq-widget-item">\
+	 <a class="dsq-widget-thread" href="http://korben.info/recharge-sans-fil-diy.html">Comment recharger son téléphone par induction magnétique (sans fil)</a>\
+	 <p class="dsq-widget-meta">26 comments &middot; 16 hours ago</p>\
+	 </li>\
+	 <li class="dsq-widget-item">\
+	 <a class="dsq-widget-thread" href="http://korben.info/ios7-vous-allez-pouvoir-changer-vos-cables-apple.html">iOS7 &#8211; Vous allez pouvoir changer vos câbles Apple</a>\
+	 <p class="dsq-widget-meta">172 comments &middot; 2 days ago</p>\
+	 </li>\
+	 <li class="dsq-widget-item">\
+	 <a class="dsq-widget-thread" href="http://korben.info/incognito-cyanogen.html">Cyanogenmod incognito</a>\
+	 <p class="dsq-widget-meta">32 comments &middot; 16 hours ago</p>\
+	 </li>\
+	 <li class="dsq-widget-item">\
+	 <a class="dsq-widget-thread" href="http://korben.info/interview-black-hat.html">Dans la peau d&rsquo;un hacker black hat</a>\
+	 <p class="dsq-widget-meta">162 comments &middot; 3 days ago</p>\
+	 </li>\
+	 </ul>\
+');
